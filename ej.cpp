51cfd36022df9516240d107dbe6f1decb4f34adb
montilla95 <brayan.montilla@correounivalle.edu.co>
2017-02-17 09:17:05 -0500

    ”ejemplo”

---
+++ ej.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+
+using namespace std;
+
+int  main () {
+	
+	int x;
+	
+	cout <<"ingrese algo"<<endl;
+	cin >> x ;
+	
+	switch(x){
+		case 1:
+		  cout << "sooy el primero"<< endl;
+		  break;
+		  
+		  case 2:
+		  cout <<"soy el segundo"<<endl;
+		  break;
+		  
+		  default:
+		  cout<<"no soy nadie"<<endl;
+		  break;
+		  		  }
+	 }
+	
+	 
+
